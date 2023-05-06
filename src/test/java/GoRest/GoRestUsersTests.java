b102e8316de7c2a9d63a1989dbb596ac6a369782
Gökhan Eren Kamer <95923887+erengk@users.noreply.github.com>
2023-05-06 09:41:13 +0300

    Add files via upload

---
+++ src/test/java/GoRest/GoRestUsersTests.java
@@ -0,0 +1,186 @@
+package GoRest;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class GoRestUsersTests {
+    Faker randomUretici = new Faker();
+    int userID;
+
+    RequestSpecification reqSpec;
+
+    @BeforeClass
+    public void setup() {
+
+        baseURI = "https://gorest.co.in/public/v2/users";  //baseURI RequestSpecification dan önce tanımlanmalı
+        //baseURI ="https://test.gorest.co.in/public/v2/users/";
+
+        reqSpec = new RequestSpecBuilder()
+                .addHeader("Authorization", "Bearer 9b836f892ac8c62aa84232a0701b07ba171a8033d220bef70bfeb858f4e893e2")
+                .setContentType(ContentType.JSON)
+                .build();
+
+    }
+
+
+    @Test(enabled = false)
+    public void createUserJson() {
+        // POST https://gorest.co.in/public/v2/users
+        // "Authorization: Bearer 523891d26e103bab0089022d20f1820be2999a7ad693304f560132559a2a152d"
+        // {"name":"{{$randomFullName}}", "gender":"male", "email":"{{$randomEmail}}", "status":"active"}
+        String rndFullname = randomUretici.name().fullName();
+        String rndEmail = randomUretici.internet().emailAddress();
+
+        userID =
+                given()
+                        .spec(reqSpec)
+                        .body("{\"name\":\"" + rndFullname + "\", \"gender\":\"male\", \"email\":\"" + rndEmail + "\", \"status\":\"active\"}")
+                        //.log().uri()
+                        //.log().body()
+
+                        .when()
+                        .post("")
+
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .contentType(ContentType.JSON)
+                        .extract().path("id");
+    }
+
+    @Test
+    public void createUserMap() {
+
+        System.out.println("baseURI = " + baseURI);
+        String rndFullname = randomUretici.name().fullName();
+        String rndEmail = randomUretici.internet().emailAddress();
+
+        Map<String, String> newUser = new HashMap<>();
+        newUser.put("name", rndFullname);
+        newUser.put("gender", "male");
+        newUser.put("email", rndEmail);
+        newUser.put("status", "active");
+
+        userID =
+                given()
+                        .header("Authorization", "Bearer 9b836f892ac8c62aa84232a0701b07ba171a8033d220bef70bfeb858f4e893e2")
+                        .contentType(ContentType.JSON)
+                        .body(newUser)
+                        .log().uri()
+                        //.log().body()
+
+                        .when()
+                        .post("")
+
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .contentType(ContentType.JSON)
+                        .extract().path("id");
+    }
+
+    @Test(enabled = false)
+    public void createUserClass() {
+        String rndFullname = randomUretici.name().fullName();
+        String rndEmail = randomUretici.internet().emailAddress();
+
+        User newUser = new User();
+        newUser.name = rndFullname;
+        newUser.gender = "male";
+        newUser.email = rndEmail;
+        newUser.status = "active";
+
+        userID =
+                given()
+                        .spec(reqSpec)
+                        .body(newUser)
+                        //.log().uri()
+                        //.log().body()
+
+                        .when()
+                        .post("")
+
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .contentType(ContentType.JSON)
+                        .extract().path("id");
+    }
+
+    @Test(dependsOnMethods = "createUserMap")
+    public void getUserByID() {
+
+        given()
+                .spec(reqSpec)
+
+                .when()
+                .get("" + userID)
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id", equalTo(userID))
+        ;
+    }
+
+    @Test(dependsOnMethods = "getUserByID")
+    public void updateUser() {
+
+        Map<String, String> updateUser = new HashMap<>();
+        updateUser.put("name", "GokhanEren Kamer");
+
+        given()
+                .spec(reqSpec)
+                .body(updateUser)
+
+                .when()
+                .put("" + userID)
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("id", equalTo(userID))
+                .body("name", equalTo("GokhanEren Kamer"))
+        ;
+    }
+
+    @Test(dependsOnMethods = "updateUser")
+    public void deleteUser() {
+
+        given()
+
+                .spec(reqSpec)
+                .when()
+                .delete("" +userID)
+
+                .then()
+                .log().body()
+                .statusCode(204)
+
+        ;
+    }
+
+    @Test(dependsOnMethods = "deleteUser")
+    public void deleteUserNegative() {
+        given()
+                .spec(reqSpec)
+                .when()
+                .delete("" +userID)
+
+                .then()
+                .log().all()
+                .statusCode(404)
+        ;
+    }
+}
